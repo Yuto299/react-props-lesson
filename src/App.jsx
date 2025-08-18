230ac8b28e6db6ab16ab410d81359efed6283345
Yuto299 <ota.se.ke@outlook.jp>
2025-08-18 23:57:48 +0900

    一つは完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import "./App.css";
 
+function Card(props) {
+  return (
+    <>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar.img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+
+      <h2>Jack Bauer</h2>
+      <img
+        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        alt="avatar_img"
+      />
+      <p>+987 654 321</p>
+      <p>jack@nowhere.com</p>
+
+      <h2>Chuck Norris</h2>
+      <img
+        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        alt="avatar_img"
+      />
+      <p>+918 372 574</p>
+      <p>gmail@chucknorris.com</p>
+    </>
+  );
 }
 
 export default App;
