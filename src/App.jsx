169f8294edbc3269f7ecaba40cffe3251e80a6ec
Yuto299 <ota.se.ke@outlook.jp>
2025-08-19 00:05:39 +0900

    全ての実装は完了

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,19 @@ function App() {
         email="b@beyonce.com"
       />
 
-      <h2>Jack Bauer</h2>
-      <img
-        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        alt="avatar_img"
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
       />
-      <p>+987 654 321</p>
-      <p>jack@nowhere.com</p>
 
-      <h2>Chuck Norris</h2>
-      <img
-        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        alt="avatar_img"
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="++918 372 574"
+        email="gmail@chucknorris.com"
       />
-      <p>+918 372 574</p>
-      <p>gmail@chucknorris.com</p>
     </>
   );
 }
